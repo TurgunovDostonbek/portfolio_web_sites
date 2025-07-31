b06d1d72a89d01545e9d9677af56b227d8671b4b
TurgunovDostonbek <turgunovdostonbek@gmail.com>
2025-07-31 12:10:32 +0500

    end

--- src/contents/project.ts
+++ src/contents/project.ts
@@ -1,69 +1,72 @@
 import { Project } from "@/types";
 
-
 export const projects: Project[] = [
-    {
-        title: 'E-commerce Platform',
-        description: 'A full-stack e-commerce platform built with Next.js, TypeScript, and Tailwind CSS.',
-        technologies: ['Next.js', 'TypeScript', 'Tailwind CSS', 'Stripe'],
-        githubLink: 'https://github.com',
-        demoLink: 'https://demo.com',
-        image: '/projects/e-commerce-website.png',
-      },
-      {
-        title: 'Task Management App',
-        description: 'A collaborative task management application with real-time updates.',
-        technologies: ['React', 'Node.js', 'MongoDB', 'Socket.io'],
-        githubLink: 'https://github.com',
-        demoLink: 'https://demo.com',
-        image: '/projects/task-manager.webp',
-      },
-      {
-        title: 'Portfolio Website',
-        description: 'My personal portfolio website showcasing my projects and skills.',
-        technologies: ['Next.js', 'Tailwind CSS', 'Framer Motion'],
-        githubLink: 'https://github.com',
-        demoLink: 'https://demo.com',
-        image: '/projects/portfolio-website.jpg',
-      },
-      {
-        title: 'Blog Platform',
-        description: 'A blogging platform with user authentication and markdown support.',
-        technologies: ['Gatsby', 'GraphQL', 'Contentful'],
-        githubLink: 'https://github.com',
-        demoLink: 'https://demo.com',
-        image: '/projects/blog-website.jpeg',
-      },
-      {
-        title: 'Weather App',
-        description: 'A weather application that provides real-time weather updates.',
-        technologies: ['React', 'OpenWeatherMap API'],
-        githubLink: 'https://github.com',
-        demoLink: 'https://demo.com',
-        image: '/projects/weather-app.png',
-      },
-      {
-        title: 'Chat Application',
-        description: 'A real-time chat application using WebSocket technology.',
-        technologies: ['React', 'Node.js', 'Socket.io'],
-        githubLink: 'https://github.com',
-        demoLink: 'https://demo.com',
-        image: '/projects/chat-app.png',
-      },
-      {
-        title: 'Recipe Finder',
-        description: 'A recipe search application using the Edamam API.',
-        technologies: ['React', 'Edamam API'],
-        githubLink: 'https://github.com',
-        demoLink: 'https://demo.com',
-        image: '/projects/recipe-finder.png',
-      },
-      {
-        title: 'Expense Tracker',
-        description: 'A personal finance tracker to manage expenses and income.',
-        technologies: ['React', 'Firebase'],
-        githubLink: 'https://github.com',
-        demoLink: 'https://demo.com',
-        image: '/projects/expense-tracker.webp',
-      }
-  ];
+  {
+    title: "E-commerce Platform",
+    description:
+      "A full-stack e-commerce platform built with JavaScript and React.",
+    technologies: ["CSS", "JavaScript", "React"],
+    githubLink: "https://magazin-shopping.vercel.app/",
+    demoLink: "https://magazin-shopping.vercel.app/",
+    image: "/projects/ShoppingWebsites.png",
+  },
+
+  {
+    title: "Portfolio Website",
+    description:
+      "My personal portfolio website showcasing my projects and skills.",
+    technologies: ["Next.js", "Tailwind CSS", "Framer Motion"],
+    githubLink: "https://portfolio1398.vercel.app/",
+    demoLink: "https://portfolio1398.vercel.app/",
+    image: "/projects/portfolio.png",
+  },
+  {
+    title: "Blog Platform Boosted",
+    description:
+      "A blogging platform with user authentication and markdown support.",
+    technologies: ["HTML", "CSS", "JavaScript"],
+    githubLink: "https://business-website1398.vercel.app/",
+    demoLink: "https://business-website1398.vercel.app/",
+    image: "/projects/Bussnise.png",
+  },
+  {
+    title: "Youtube Clone",
+    description: "Youtube Clone",
+    technologies: ["CSS", "JS", "API", "React"],
+    githubLink: "https://you-tube-clone-omega-neon.vercel.app/",
+    demoLink: "https://you-tube-clone-omega-neon.vercel.app/",
+    image: "/projects/youtubeClone.png",
+  },
+  {
+    title: "Movie Play",
+    description: "Movies Plays",
+    technologies: ["React"],
+    githubLink: "https://kinoplay.vercel.app/",
+    demoLink: "https://kinoplay.vercel.app/",
+    image: "/projects/kinoPlay.png",
+  },
+  {
+    title: "Countries Crud",
+    description: "Information About Countries",
+    technologies: ["HTML", "CSS", "JavaScript"],
+    githubLink: "https://countries-crud.vercel.app/",
+    demoLink: "https://countries-crud.vercel.app/",
+    image: "/projects/CountriesCrud.png",
+  },
+  {
+    title: "Skateboards Info",
+    description: "Welcome to Boosted USA",
+    technologies: ["HTML", "CSS"],
+    githubLink: "https://skateboards1398.vercel.app/",
+    demoLink: "https://skateboards1398.vercel.app/",
+    image: "/projects/Block web site boosted.png",
+  },
+  // {
+  //   title: "Expense Tracker",
+  //   description: "A personal finance tracker to manage expenses and income.",
+  //   technologies: ["React", "Firebase"],
+  //   githubLink: "https://github.com",
+  //   demoLink: "https://demo.com",
+  //   image: "/projects/expense-tracker.webp",
+  // },
+];
